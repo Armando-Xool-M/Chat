17cd6af097337f60340571bb52d70f6eea1b68ec
Armando-Xool-M <94819656+Armando-Xool-M@users.noreply.github.com>
2023-03-06 12:33:49 -0600

    Add files via upload

---
+++ Chat/ChatPrueba/src/Cliente/HiloCliente.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cliente;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author CHARL
+ */
+public class HiloCliente extends Thread {
+
+    private DataInputStream entrada;
+    private Socket Socketcliente;
+    private Cliente cliente;
+
+    public HiloCliente(Socket Scliente, Cliente cliente) {
+        this.Socketcliente = Scliente;
+        this.cliente = cliente;
+     
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                entrada = new DataInputStream(Socketcliente.getInputStream());
+                cliente.mensaje(entrada.readUTF());
+                cliente.conectados(entrada.readInt());
+            } catch (IOException ex) {
+                Logger.getLogger(HiloCliente.class.getName()).log(Level.SEVERE, null, ex);
+                }
+        }
+    }
+
+   
+    
+}
